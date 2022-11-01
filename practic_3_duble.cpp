3391ec46cdd49d02a8cacf2eeab698eae523504d
Oktawn <belyhmitia@gmail.com>
2022-11-01 23:09:11 +0500

    rr

--- practic_3_duble.cpp
+++ practic_3_duble.cpp
@@ -5,26 +5,53 @@ int main()
 {
 	srand((unsigned int)(time(NULL)));
 	const int size = 100000;
+	cout << "size: " << size << "\n";
 	int ar[size];
-	ar_create(ar, size);
-	//ar_show(ar, size);
+	ar_create_best(ar, size);
 	auto start = steady_clock::now();
-	selest_sort(ar, size);
+	//selest_sort(ar, size);
+	quick_sort(ar, size);
 	auto end = steady_clock::now();
 	duration <double> elapsed = end - start;
-	cout << "select sort time= " << elapsed.count() << "\n";
-	//ar_show(ar, size);
 	cout << "\n";
+	cout << "swap= " << swaps << endl;
+	cout << "compare= " << comparer << endl;
+	//cout << "select sort time= " << elapsed.count() << "\n";
+	cout << "quick sort time= " << elapsed.count() << "\n";
+
 	ar_create(ar, size);
-	//ar_show(ar, size);
+
 	start = steady_clock::now();
+	//selest_sort(ar, size);
 	quick_sort(ar, size);
 	end = steady_clock::now();
 	elapsed = end - start;
+	cout << "\n";
+	cout << "swap= " << swaps << endl;
+	cout << "compare= " << comparer << endl;
+	//cout << "select sort time= " << elapsed.count() << "\n";
 	cout << "quick sort time= " << elapsed.count() << "\n";
-	//ar_show(ar, size);
 
-	//delete[] &ar;
+	ar_create_bad(ar, size);
+	start = steady_clock::now();
+	//selest_sort(ar, size);
+	quick_sort(ar, size);
+	end = steady_clock::now();
+	elapsed = end - start;
+	cout << "\n";
+	cout << "swap= " << swaps << endl;
+	cout << "compare= " << comparer << endl;
+	//cout << "select sort time= " << elapsed.count() << "\n";
+	cout << "quick sort time= " << elapsed.count() << "\n";
+
+	//ar_create(ar, size);
+	////ar_show(ar, size);
+	//start = steady_clock::now();
+	//quick_sort(ar, size);
+	//end = steady_clock::now();
+	//elapsed = end - start;
+	//cout << "quick sort time= " << elapsed.count() << "\n";
+	//ar_show(ar, size);
 
 	return 0;
 
--- sort.h
+++ sort.h
@@ -7,7 +7,14 @@
 using namespace std;
 using namespace chrono;
 
+
+long long int swaps;
+long long int comparer;
+
+
 void ar_create(int ar[], int size);
+void ar_create_bad(int ar[], int size);
+void ar_create_best(int ar[], int size);
 void ar_show(int ar[], int size);
 
 void selest_sort(int ar[], int size);
@@ -19,7 +26,19 @@ void quick_sort(int ar[], int size);
 inline void ar_create(int ar[], int size)
 {
 	for (int i = 0; i < size; i++)
-		ar[i] = rand();
+		ar[i] = 0 + rand() % (size - 0 + 1);
+}
+
+inline void ar_create_bad(int ar[], int size)
+{
+	for (int i = 0; i < size; i++)
+		ar[i] = INT_MAX - i;
+}
+
+inline void ar_create_best(int ar[], int size)
+{
+	for (int i = 0; i < size; i++)
+		ar[i] = i;
 }
 
 inline void ar_show(int ar[], int size)
@@ -31,6 +50,8 @@ inline void ar_show(int ar[], int size)
 
 inline void selest_sort(int ar[], int size)
 {
+	int swap=0;
+	int compare=0;
 	int temp;
 	for(int i=0;i<size;i++)
 		for (int j = i + 1; j < size; j++)
@@ -40,18 +61,23 @@ inline void selest_sort(int ar[], int size)
 				temp = ar[i];
 				ar[i] = ar[j];
 				ar[j] = temp;
+				swap++;	
 			}
+			compare++;
 		}
+	swaps = swap;
+	comparer = compare;
 }
 
 inline void quick_sort(int ar[], int size)
 {
+	int swap = 0, compare = 0;
 	int left = 0; int right = size - 1; int mid = ar[size / 2];
 	int temp;
 	do
 	{
-		while (ar[left] < mid) left++;
-		while (ar[right] > mid) right--;
+		while (ar[left] < mid) { left++; compare++; }
+		while (ar[right] > mid) { right--; compare++;}
 		if (left <= right)
 		{
 			temp = ar[left];
@@ -59,8 +85,11 @@ inline void quick_sort(int ar[], int size)
 			ar[right] = temp;
 			left++;
 			right--;
+			compare++; swap++;
 		}
 	} while (left <= right);
 	if (right > 0) quick_sort(ar, right + 1);
 	if (left < size) quick_sort(&ar[left], size - left);
+	comparer = compare;
+	swaps = swap;
 }
